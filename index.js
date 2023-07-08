f679bff4214e23deb34a14946731d8a53aa187de
Nico <118764372+NicooLGH@users.noreply.github.com>
2023-07-08 14:28:52 +0200

    Animation et eval()

---
+++ index.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll(".button");
+const calcul = document.getElementById("calcul");
+const arrow = document.querySelector(".fa-arrow-left");
+const egal = document.getElementById("egal");
+const reset = document.getElementById("reset");
+
+// Animation
+
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    button.style.background = "#5e5e5e";
+  });
+  button.addEventListener("mouseup", () => {
+    button.style.background = "#1f1f1f";
+  });
+  button.addEventListener("mouseover", () => {
+    button.style.background = "#3c3c3c";
+  });
+  button.addEventListener("mouseleave", () => {
+    button.style.background = "#1f1f1f";
+  });  
+  button.addEventListener("click", (e) => {
+    calcul.textContent += e.target.id;
+    console.log(calcul.textContent);
+  });
+});
+
+arrow.addEventListener("click", () => {
+  calcul.textContent = calcul.textContent.substring(0, calcul.textContent.length - 1);
+  console.log(calcul.textContent);
+});
+
+egal.addEventListener("click", () => {
+  calcul.textContent = eval(calcul.textContent);
+});
+
+reset.addEventListener("click", () => {
+  calcul.textContent = "";
+});
+
